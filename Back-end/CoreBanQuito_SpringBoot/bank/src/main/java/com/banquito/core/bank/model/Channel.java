32250597e8e9234d4d2f3dcaccff05ce8c98e2d6
Freddy Sebastian Cárdenas Rosero <fscardenas1@espe.edu.ec>
2024-06-07 15:37:59 -0500

    Bank & Product
    Creación de Bank y Product todavía faltan de terminar

---
+++ Back-end/CoreBanQuito_SpringBoot/bank/src/main/java/com/banquito/core/bank/model/Channel.java
@@ -0,0 +1,70 @@
+package com.banquito.core.bank.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "CHANNEL")
+
+public class Channel {
+
+    @Id
+    @Column(name = "CODE_CHANNEL", length = 10, nullable = false)
+    private String code;
+    @Column(name = "CODE_BANK", length = 20, nullable = false)
+    private String codeBank;
+    @Column(name = "NAME", length = 20, nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "CODE_BANK", referencedColumnName = "CODE_BANK", insertable = false, updatable = false)
+    private Bank bank;
+
+    public Channel(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((code == null) ? 0 : code.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Channel other = (Channel) obj;
+        if (code == null) {
+            if (other.code != null)
+                return false;
+        } else if (!code.equals(other.code))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+}
