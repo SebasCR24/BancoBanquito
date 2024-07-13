24c0615700e235e00d107ba839d3c8392dacfc59
Freddy Sebastian Cárdenas Rosero <fscardenas1@espe.edu.ec>
2024-07-12 19:44:02 -0500

    Code igual al del inge
    Code Update

--- Back-end/clientdoc/src/main/java/com/banquito/core/clientdoc/service/SegmentService.java
+++ Back-end/clientdoc/src/main/java/com/banquito/core/clientdoc/service/SegmentService.java
@@ -1,7 +1,6 @@
 package com.banquito.core.clientdoc.service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
