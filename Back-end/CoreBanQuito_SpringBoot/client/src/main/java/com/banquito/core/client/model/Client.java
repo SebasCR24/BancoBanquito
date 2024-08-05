f95f9b54fc99144b4ea5b8de47de10318a08e999
mjvizuete2 <majovizuete24@gmail.com>
2024-08-05 14:33:43 -0500

    subida a amplify

--- Back-end/CoreBanQuito_SpringBoot/client/src/main/java/com/banquito/core/client/model/Client.java
+++
@@ -1,5 +0,0 @@
-package com.banquito.core.client.model;
-
-public class Client {
-
-}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   // CoreCobros_commission : 'http://corecobros-commission.us-east-2.elasticbeanstalk.com',
   // CoreCobros_invoice : 'http://corecobros-invoice.us-east-1.elasticbeanstalk.com',
   // CoreCobros_receivables : 'http://corecobros-receivables.us-east-1.elasticbeanstalk.com',
-  Core_account: 'http://localhost:8082/Account-Microservice/api/v1',
+  Core_account: 'https://m4b60phktl.execute-api.us-east-1.amazonaws.com/banquito/account-microservice/api/v1',
   //CoreCobros_company : 'http://localhost:8081/api/v1',
   CoreCobros_company : 'https://m4b60phktl.execute-api.us-east-1.amazonaws.com/banquito/company-microservice/api/v1',
   CoreCobros_cobro : 'http://localhost:8080/api/v1',
