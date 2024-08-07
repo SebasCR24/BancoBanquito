5e84a62a8f4472fa35761552c82d04182cf0599b
mjvizuete2 <majovizuete24@gmail.com>
2024-08-07 17:05:40 -0500

    cambios para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
 
 
-  Core_account: 'https://m4b60phktl.execute-api.us-east-1.amazonaws.com/banquito/account-microservice/api/v1',
-  CoreCobros_company : 'https://m4b60phktl.execute-api.us-east-1.amazonaws.com/banquito/company-microservice/api/v1',
-  CoreCobros_cobro : 'http://localhost:8080/api/v1',
+  CoreCobros_cobro : 'https://08zhnuj863.execute-api.us-east-1.amazonaws.com/banquito/order-microservice/api/v1',
+  CoreCobros_company : 'https://08zhnuj863.execute-api.us-east-1.amazonaws.com/banquito/company-microservice/api/v1',
+  Core_account: 'https://08zhnuj863.execute-api.us-east-1.amazonaws.com/banquito/account-microservice/api/v1',
 
 };
  
